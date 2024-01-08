d050646392ca80aac49b0a5583398ccc62d2ea79
Pooja <poojaghanghas479@gmail.com>
2024-01-08 22:37:20 +0530

    API call made & fetched data of restaurants

--- src/Components/Body/index.js
+++ src/Components/Body/index.js
@@ -1,6 +1,19 @@
+import { useEffect } from "react"
 import { RestaurantCard } from "./RestaurantCard"
 
 export const Body = () => {
+
+  useEffect(() => {
+    fetchData();
+    console.log("useEffect called")
+  },[])
+
+  const fetchData = async () => {
+    const data = await fetch("https://www.swiggy.com/api/seo/getListing?lat=28.389008871193543&lng=76.98319293301593")
+    const json = await data.json();
+    console.log(json);
+  }
+
   return (
     <div className="flex">
         <RestaurantCard name="Meghana Foods"/>
